355d868a5d067f014cb84a5e7c4d6dd5289385dd
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-10-13 09:17:14 +0000

    Ultimos detalles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,41 +9,42 @@ const TrafficLight = () => {
 
   const ColorChange = () => {
     if (color === "red") setColor("yellow");
-  else if (color === "yellow") setColor("green");
-  else if (color === "green") setColor("red");
+    else if (color === "yellow") setColor("green");
+    else if (color === "green") setColor("red");
   };
 
-//Utilizo un temporizador para que el color cambie solo cada cinco segundos
-   /* useEffect(() => {
-      const colorRotation = setInterval(() => {
-        ColorChange()
-      }, 2000);
-      return () => {
-        clearInterval(colorRotation)
-      };
-    }, [])
-  */
+  //Utilizo un temporizador para que el color cambie solo cada cinco segundos
+  /* useEffect(() => {
+     const colorRotation = setInterval(() => {
+       ColorChange()
+     }, 2000);
+     return () => {
+       clearInterval(colorRotation)
+     };
+   }, [])
+ */
 
   return (
-    <div className="container">
-      <div className="traffic-light">
-        <div className={`circle red ${color === "red" && "activered"}`}
-          onClick={() => setColor("red")}>
+    <div className="container-fluid">
+      <div className="traffic">
+        <div className="head traffic-light">
+          <div className={`circle red ${color === "red" && "activered"}`}
+            onClick={() => setColor("red")}>
+          </div>
+          <div className={`circle yellow ${color === "yellow" && "activeyellow"}`}
+            onClick={() => setColor("yellow")}>
+          </div>
+          <div className={`circle green ${color === "green" && "activegreen"}`}
+            onClick={() => setColor("green")}>
+          </div>
         </div>
-        <div className={`circle yellow ${color === "yellow" && "activeyellow"}`}
-          onClick={() => setColor("yellow")}>
-        </div>
-        <div className={`circle green ${color === "green" && "activegreen"}`}
-          onClick={() => setColor("green")}>
+        <div className="stick">
+          <div className="warning"></div>
+          <div className="shift-button">
+            <button onClick={ColorChange}><FontAwesomeIcon icon={faArrowUp} /></button>
+          </div>
         </div>
       </div>
-      <div className="stick">
-        <div className="warning"></div>
-        <div className="shift-button">
-          <button onClick={ColorChange}><FontAwesomeIcon icon={faArrowUp}/></button>
-        </div>
-      </div>
-      
     </div>
   );
 };
